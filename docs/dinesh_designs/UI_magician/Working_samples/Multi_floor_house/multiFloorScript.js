be9bd29196550202a9f1971005cc906266677bf8
dinguluer <din.gulu.er@gmail.com>
2014-05-05 12:40:00 +0530

    Dinesh : Multifloor : Added support for temperature measurement

--- docs/dinesh_designs/UI_magician/Working_samples/Multi_floor_house/multiFloorScript.js
+++ docs/dinesh_designs/UI_magician/Working_samples/Multi_floor_house/multiFloorScript.js
@@ -13,8 +13,29 @@ function doc_onload()
    floor_area_visible_onload();
    // Show widget on load
    Central_widget_visible_onload();
-   // Create the Socket
+   // Create the Socket for I/O devices
    create_device_socket(Multi_Floor_Device_Array);
+   // Create the Socket for Measurement  devices
+   create_measurement_device_socket(Multi_Floor_Temperature_Device_Array);
+}
+
+function create_measurement_device_socket( Device_Array )
+{
+
+    for (var i = 0; i < Device_Array.length; i++)
+    {
+        var btn = new vscpws_simpleTextEvent( Device_Array[i].url,           // url to VSCP websocket i/f
+                                             Device_Array[i].id,              // Where it should be placed
+                                             Device_Array[i].vscpclass,       // Event class 10/60/65/15
+                                             Device_Array[i].vscptype,        // Evet type
+                                             Device_Array[i].codingIndex,     // Datacoding index
+                                             Device_Array[i].decimals,        // Number of decimals
+                                             Device_Array[i].formatstr,       // A value format string
+                                             Device_Array[i].guid,            // GUID we are interested in
+                                             Device_Array[i].fncallback );     // If set function to call
+                                                                               // when data arraives
+    }
+
 }
 
 function create_device_socket( Device_Array )
