e13749bac78fb719d050a472d730b16fe777ff5f
Ake Hedman <akhe@grodansparadis.com>
2014-09-05 17:17:33 +0200

    New default port 8080 for samples default auth informtionm added jquery files added

--- testws/settings.js
+++ testws/settings.js
@@ -1,3 +1,7 @@
 
+var user = "admin";
+var authdomain = "mydomain.com";
+var passwordhash = "d50c3180375c27927c22e42a379c3f67";  // hash on "user:authdomain:password"
+
 // Set the server where you have websockets installed.
-var VSCP_WEBSOCKET_URL="ws://192.168.1.20:7681";
+var VSCP_WEBSOCKET_URL="ws://192.168.1.8:8080";
