8b117697b7b9e6f4166031a28702064c36776e31
Ake Hedman <akhe@grodansparadis.com>
2016-02-15 17:31:12 +0100

    More copyright

--- testws/settings.js
+++ testws/settings.js
@@ -10,5 +10,5 @@ var settings = {
     passwordHash: "d50c3180375c27927c22e42a379c3f67", // Hash on "user:authdomain:password"
     //url: "ws://192.168.1.8:8080" // Non SSL (to be able to use from remote machine)
     //url: "wss://192.168.1.8:8080" // SSL (to be able to use from remote machine)
-    url: "ws://demo.vscp.org:8080" // Demoserver
+    url: "ws://192.168.1.9:8080" // Demoserver
 };
