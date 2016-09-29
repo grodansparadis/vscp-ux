e57544cc435b599d8268de00917f6fc7498b059f
Ake Hedman <akhe@grodansparadis.com>
2016-09-29 23:51:23 +0200

    Fixed readvar in REST

--- js/settings.js
+++ js/settings.js
@@ -7,8 +7,8 @@ var settings = {
 
     user: "admin",
     password: "secret",
-    vscptoken: "Stay Hungry. Stay Foolish.",
-    passwordHash: "13ca88de01ce06e377f74e61c23f630b", // Hash on "user:vscptoken:password"
+    authdomain: "mydomain.com",
+    passwordHash: "d50c3180375c27927c22e42a379c3f67", // Hash on "user:vscptoken:password"
     //url: "ws://192.168.1.9:8080" // Non SSL (to be able to use from remote machine)
     //url: "wss://192.168.1.9:8080" // SSL (to be able to use from remote machine)
     //url: "ws://demo.vscp.org:8080" // Demoserver
