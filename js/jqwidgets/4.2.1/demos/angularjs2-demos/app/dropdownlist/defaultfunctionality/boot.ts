f01cc666404c5f7e28a63414779fabd9756f4a8a
Ake Hedman <akhe@grodansparadis.com>
2016-12-14 15:11:03 +0100

    Removed jqwidgets

--- js/jqwidgets/4.2.1/demos/angularjs2-demos/app/dropdownlist/defaultfunctionality/boot.ts
+++
@@ -1,5 +0,0 @@
-import {enableProdMode} from '@angular/core';
-enableProdMode();
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-bootstrap(AppComponent);
