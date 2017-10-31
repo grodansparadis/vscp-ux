b0bac8a7c20bcf96e6226be8516af6724fc7d717
Ake Hedman <akhe@grodansparadis.com>
2017-10-31 10:46:29 +0100

    Changed menu back to non inverted again

--- js/settings.js
+++ js/settings.js
@@ -14,8 +14,8 @@ var settings = {
     authdomain: "mydomain.com",
     passwordHash: "d50c3180375c27927c22e42a379c3f67", // Hash on "user:vscptoken:password"
     vscpkey: "A4A86F7D7E119BA3F0CD06881E371B989B33B6D606A863B633EF529D64544F8E",
-    //url: "ws://192.168.1.9:8080" // Non SSL (to be able to use from remote machine)
-    //url: "wss://192.168.1.9:8080" // SSL (to be able to use from remote machine)
-    //url: "ws://demo.vscp.org:8080" // Demoserver
+    //url: "ws://192.168.1.9:8884/ws1" // Non SSL (to be able to use from remote machine)
+    //url: "wss://192.168.1.9:8884/ws1" // SSL (to be able to use from remote machine)
+    //url: "ws://demo.vscp.org:8884/ws1" // Demoserver
     url: "ws://localhost:8884/ws1" // Local server
 };
