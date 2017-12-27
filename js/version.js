0d56c1686c46f989b56262ed67cc9c83d459c969
BlueAndi <vscp@blue-andi.de>
2017-12-28 00:25:14 +0100

    License and namespace 'vscp_ux' added to version.js

--- js/version.js
+++ js/version.js
@@ -1,12 +1,49 @@
-// settings.js
-// ===========
-// This file contains settings for VSCP websocket and other JavaScript examples
-// Version information comes from vscp.js
+//
+// Version information
+// Copyright (C) 2012-2017 Ake Hedman, Grodans Paradis AB
+// <akhe@grodansparadis.com>
+// Copyright (c) 2017 Andreas Merkle
+// <vscp@blue-andi.de>
+//
+// Licence:
+// The MIT License (MIT)
+// [OSI Approved License]
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2012-2017 Grodans Paradis AB (Paradise of the Frog)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Alternative licenses for VSCP & Friends may be arranged by contacting
+// Grodans Paradis AB at info@grodansparadis.com, http://www.grodansparadis.com
+//
 
-var vscp_ux_version = {
-    major: vscp.version.major,
-    minor: vscp.version.minor,
-    sub: vscp.version.release,
-    full: ""+vscp.version.major+"."+vscp.version.minor+"."+vscp.version.release,
-    copyright: "Copyright (c) 2012-2017 Grodans Paradis AB (Paradise of the Frog)"
+/** Get VSCP UX version.
+ * @return {object} VSCP UX version object
+ */
+var vscp_ux = {
+	version: {
+        major: vscp.version.major,
+        minor: vscp.version.minor,
+        sub: vscp.version.release,
+        full: ""+vscp.version.major+"."+vscp.version.minor+"."+vscp.version.release,
+        copyright: "Copyright (c) 2012-2017 Grodans Paradis AB (Paradise of the Frog)"
+    }
 };
