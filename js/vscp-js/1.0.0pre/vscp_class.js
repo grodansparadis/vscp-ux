e36bbf99233e721f5f1fd445421d5733997fb0e1
BlueAndi <vscp@blue-andi.de>
2018-07-07 23:11:05 +0200

    VSCP class and type definitions are moved to separate files (autogenerated).

---
+++ js/vscp-js/1.0.0pre/vscp_class.js
@@ -0,0 +1,116 @@
+// VSCP class definition file
+//
+// Copyright (C) 2012-2018 Ake Hedman, Grodans Paradis AB
+// <akhe@grodansparadis.com>
+// Copyright (c) 2015-2018 Andreas Merkle
+// <vscp@blue-andi.de>
+//
+// Licence:
+// The MIT License (MIT)
+// [OSI Approved License]
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2012-2018 Grodans Paradis AB (Paradise of the Frog)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Alternative licenses for VSCP & Friends may be arranged by contacting
+// Grodans Paradis AB at info@grodansparadis.com, http://www.grodansparadis.com
+//
+
+/*
+            !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
+                           This file is auto-generated
+                see https://github.com/grodansparadis/vscp-classes
+                        Generated: 2018-07-07 22:33:37.834511
+*/
+
+/** Namespace for all functionality of the VSCP provided libraries.
+ * @namespace vscp
+ */
+var vscp = vscp || {};
+
+/** VSCP classes
+ * @enum {number}
+ * @const
+ */
+vscp.constants.classes = {
+
+    CLASS1_PROTOCOL: 0,
+    CLASS1_ALARM: 1,
+    CLASS1_SECURITY: 2,
+    CLASS1_MEASUREMENT: 10,
+    CLASS1_DATA: 15,
+    CLASS1_INFORMATION: 20,
+    CLASS1_CONTROL: 30,
+    CLASS1_MULTIMEDIA: 40,
+    CLASS1_AOL: 50,
+    CLASS1_MEASUREMENT64: 60,
+    CLASS1_MEASUREZONE: 65,
+    CLASS1_MEASUREMENT32: 70,
+    CLASS1_SETVALUEZONE: 85,
+    CLASS1_WEATHER: 90,
+    CLASS1_WEATHER_FORECAST: 95,
+    CLASS1_PHONE: 100,
+    CLASS1_DISPLAY: 102,
+    CLASS1_IR: 110,
+    CLASS1_GNSS: 206,
+    CLASS1_WIRELESS: 212,
+    CLASS1_DIAGNOSTIC: 506,
+    CLASS1_ERROR: 508,
+    CLASS1_LOG: 509,
+    CLASS1_LABORATORY: 510,
+    CLASS1_LOCAL: 511,
+    CLASS2_LEVEL1_PROTOCOL: 512,
+    CLASS2_LEVEL1_ALARM: 513,
+    CLASS2_LEVEL1_SECURITY: 514,
+    CLASS2_LEVEL1_MEASUREMENT: 522,
+    CLASS2_LEVEL1_DATA: 527,
+    CLASS2_LEVEL1_INFORMATION1: 532,
+    CLASS2_LEVEL1_CONTROL: 542,
+    CLASS2_LEVEL1_MULTIMEDIA: 552,
+    CLASS2_LEVEL1_AOL: 562,
+    CLASS2_LEVEL1_MEASUREMENT64: 572,
+    CLASS2_LEVEL1_MEASUREZONE: 577,
+    CLASS2_LEVEL1_MEASUREMENT32: 582,
+    CLASS2_LEVEL1_SETVALUEZONE: 597,
+    CLASS2_LEVEL1_WEATHER: 602,
+    CLASS2_LEVEL1_WEATHERFORECAST: 607,
+    CLASS2_LEVEL1_PHONE: 612,
+    CLASS2_LEVEL1_DISPLAY: 614,
+    CLASS2_LEVEL1_IR: 622,
+    CLASS2_LEVEL1_GNSS: 718,
+    CLASS2_LEVEL1_WIRELESS: 724,
+    CLASS2_LEVEL1_DIAGNOSTIC: 1018,
+    CLASS2_LEVEL1_ERROR: 1020,
+    CLASS2_LEVEL1_LOG: 1021,
+    CLASS2_LEVEL1_LABORATORY: 1022,
+    CLASS2_LEVEL1_LOCAL: 1023,
+    CLASS2_PROTOCOL: 1024,
+    CLASS2_CONTROL: 1025,
+    CLASS2_INFORMATION: 1026,
+    CLASS2_TEXT2SPEECH: 1027,
+    CLASS2_CUSTOM: 1029,
+    CLASS2_DISPLAY: 1030,
+    CLASS2_MEASUREMENT_STR: 1040,
+    CLASS2_MEASUREMENT_FLOAT: 1060,
+    CLASS2_VSCPD: 65535
+
+};
